ef3b9c782f0dc7b69b43d67ecce9065e7c666b38
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 19:35:11 -0400

    Cache v111 PIN fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cuentas-claras-v110";
+const CACHE = "cuentas-claras-v111";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,7 +18,7 @@ const ASSETS = [
   "./src/js/i18n.js?v=v103",
   "./src/js/storage.js?v=v86",
   "./src/js/migrations.js?v=v86",
-  "./src/js/state.js?v=v110",
+  "./src/js/state.js?v=v111",
   "./src/js/calculations.js?v=v96",
   "./src/js/categories.js?v=v86",
   "./src/js/work.js?v=v95",
@@ -29,7 +29,7 @@ const ASSETS = [
   "./src/js/history.js?v=v86",
   "./src/js/recommendations.js?v=v108",
   "./src/js/render.js?v=v110",
-  "./src/js/app.js?v=v110",
+  "./src/js/app.js?v=v111",
   "./src/js/theme-colors.js?v=v88",
 ];
 
