10fdb4761f3c3d5507d6b0565830f1a054e7c34e
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 19:42:27 -0400

    Release v114 with reliable local PIN verifier

--- src/js/state.js
+++ src/js/state.js
@@ -6,8 +6,8 @@ const fmt0 = (n) => Math.round(isFinite(n) ? n : 0).toLocaleString(LANG === "es"
 
 const fmt10 = (n) => (Math.round((isFinite(n) ? n : 0) / 10) * 10).toLocaleString(LANG === "es" ? "es-ES" : "en-US", { maximumFractionDigits: 0 });
 
-const APP_VERSION = "v113";
-const BUILD_DATE = "29/08 02:00 UTC";
+const APP_VERSION = "v114";
+const BUILD_DATE = "29/08 02:18 UTC";
 
 let uidCounter = 1;
 
@@ -15,6 +15,7 @@ const uid = () => "id_" + Date.now() + "_" + (uidCounter++);
 
 const PROFILES_KEY = "cuentas-claras:perfiles";
 const PIN_HASH_KEY = "cuentas-claras:pin-hash";
+const PIN_QUICK_KEY = "cuentas-claras:pin-quick-v1";
 const PIN_ATTEMPTS_KEY = "cuentas-claras:pin-attempts";
 const PIN_LOCK_KEY = "cuentas-claras:pin-lock-until";
 const BIOMETRIC_CRED_KEY = "cuentas-claras:biometric-credential";
