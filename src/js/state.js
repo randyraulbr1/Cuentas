e69a49ff0db3bc9bbdf863551469af24d62f410b
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 14:04:30 -0400

    Set app version to v97

--- src/js/state.js
+++ src/js/state.js
@@ -6,8 +6,8 @@ const fmt0 = (n) => Math.round(isFinite(n) ? n : 0).toLocaleString(LANG === "es"
 
 const fmt10 = (n) => (Math.round((isFinite(n) ? n : 0) / 10) * 10).toLocaleString(LANG === "es" ? "es-ES" : "en-US", { maximumFractionDigits: 0 });
 
-const APP_VERSION = "v96";
-const BUILD_DATE = "28/08 17:45 UTC";
+const APP_VERSION = "v97";
+const BUILD_DATE = "28/08 18:10 UTC";
 
 let uidCounter = 1;
 
