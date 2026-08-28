8325c67dd841ca9ba42ab0686c951eed622199db
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 16:10:23 -0400

    Actualizar caché PWA a v101

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cuentas-claras-v100";
+const CACHE = "cuentas-claras-v101";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,7 +18,7 @@ const ASSETS = [
   "./src/js/i18n.js?v=v95",
   "./src/js/storage.js?v=v86",
   "./src/js/migrations.js?v=v86",
-  "./src/js/state.js?v=v100",
+  "./src/js/state.js?v=v101",
   "./src/js/calculations.js?v=v96",
   "./src/js/categories.js?v=v86",
   "./src/js/work.js?v=v95",
@@ -27,7 +27,7 @@ const ASSETS = [
   "./src/js/api.js?v=v100",
   "./src/js/plaid-link.js?v=v86",
   "./src/js/history.js?v=v86",
-  "./src/js/recommendations.js?v=v99",
+  "./src/js/recommendations.js?v=v101",
   "./src/js/render.js?v=v99",
   "./src/js/app.js?v=v100",
   "./src/js/theme-colors.js?v=v88",
