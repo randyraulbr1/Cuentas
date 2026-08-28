4755e4c029b85d184de07385401856a223674ba1
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 19:37:37 -0400

    Cache v113 biometric fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cuentas-claras-v112";
+const CACHE = "cuentas-claras-v113";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,7 +18,7 @@ const ASSETS = [
   "./src/js/i18n.js?v=v103",
   "./src/js/storage.js?v=v86",
   "./src/js/migrations.js?v=v86",
-  "./src/js/state.js?v=v112",
+  "./src/js/state.js?v=v113",
   "./src/js/calculations.js?v=v96",
   "./src/js/categories.js?v=v86",
   "./src/js/work.js?v=v95",
@@ -28,7 +28,7 @@ const ASSETS = [
   "./src/js/plaid-link.js?v=v86",
   "./src/js/history.js?v=v86",
   "./src/js/recommendations.js?v=v108",
-  "./src/js/render.js?v=v112",
+  "./src/js/render.js?v=v113",
   "./src/js/app.js?v=v112",
   "./src/js/theme-colors.js?v=v88",
 ];
