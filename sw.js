00df6cf09a43cc31d0355fe2f6f8f76457e23f83
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 19:16:52 -0400

    Cache v108 assets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cuentas-claras-v107";
+const CACHE = "cuentas-claras-v108";
 const ASSETS = [
   "./",
   "./index.html",
@@ -12,13 +12,13 @@ const ASSETS = [
   "./offline.html",
   "./src/css/base.css?v=v107",
   "./src/css/components.css?v=v107",
-  "./src/css/pages.css?v=v107",
+  "./src/css/pages.css?v=v108",
   "./src/css/theme-colors.css?v=v107",
   "./src/js/icons.js?v=v95",
   "./src/js/i18n.js?v=v103",
   "./src/js/storage.js?v=v86",
   "./src/js/migrations.js?v=v86",
-  "./src/js/state.js?v=v107",
+  "./src/js/state.js?v=v108",
   "./src/js/calculations.js?v=v96",
   "./src/js/categories.js?v=v86",
   "./src/js/work.js?v=v95",
@@ -27,9 +27,9 @@ const ASSETS = [
   "./src/js/api.js?v=v104",
   "./src/js/plaid-link.js?v=v86",
   "./src/js/history.js?v=v86",
-  "./src/js/recommendations.js?v=v107",
-  "./src/js/render.js?v=v107",
-  "./src/js/app.js?v=v107",
+  "./src/js/recommendations.js?v=v108",
+  "./src/js/render.js?v=v108",
+  "./src/js/app.js?v=v108",
   "./src/js/theme-colors.js?v=v88",
 ];
 
