ef7a76648207d9f10150d5e69e72572c3814b7eb
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-28 19:37:39 -0400

    Release v113

--- src/js/state.js
+++ src/js/state.js
@@ -6,8 +6,8 @@ const fmt0 = (n) => Math.round(isFinite(n) ? n : 0).toLocaleString(LANG === "es"
 
 const fmt10 = (n) => (Math.round((isFinite(n) ? n : 0) / 10) * 10).toLocaleString(LANG === "es" ? "es-ES" : "en-US", { maximumFractionDigits: 0 });
 
-const APP_VERSION = "v112";
-const BUILD_DATE = "29/08 01:52 UTC";
+const APP_VERSION = "v113";
+const BUILD_DATE = "29/08 02:00 UTC";
 
 let uidCounter = 1;
 
