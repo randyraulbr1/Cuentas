d9276ec23c72898abdbd787ec76abfcffe556b41
randyraulbr1 <randyraulbr1@gmail.com>
2026-08-29 13:21:30 -0400

    Reorder Work screen and simplify payment flow (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cuentas-claras-v136";
+const CACHE = "cuentas-claras-v137";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,7 +18,7 @@ const ASSETS = [
   "./src/js/i18n.js?v=v129",
   "./src/js/storage.js?v=v86",
   "./src/js/migrations.js?v=v86",
-  "./src/js/state.js?v=v136",
+  "./src/js/state.js?v=v137",
   "./src/js/calculations.js?v=v133",
   "./src/js/categories.js?v=v86",
   "./src/js/work.js?v=v126",
@@ -28,7 +28,7 @@ const ASSETS = [
   "./src/js/plaid-link.js?v=v86",
   "./src/js/history.js?v=v86",
   "./src/js/recommendations.js?v=v133",
-  "./src/js/render.js?v=v133",
+  "./src/js/render.js?v=v137",
   "./src/js/app.js?v=v136",
   "./src/js/theme-colors.js?v=v88",
 ];
